deeaa99f551dad52e00b46f41be93baaebc26f9c
CodeYouness <ylijassi@gmail.com>
2024-05-11 19:26:11 +0200

    added class and innerhtml for every option of if (fizz,buzz,fizzbuzz or number) added appendchild of every option of if changed position of  pilldesignEl inside for

--- js/script.js
+++ js/script.js
@@ -4,25 +4,34 @@ const divEl = document.querySelector('#FizzBuzz-container')
 
 button.addEventListener('click', function () {
 
-
-
     for (let i = 1; i <= 100; i++) {
 
         button.classList.add('d-none')
+        let pillDesign = document.createElement('p')
+        pillDesign.classList.add('col-2', 'badge', 'rounded-pill')
 
         if ((i % 3 === 0) && (i % 5 !== 0)) {
-            let pillDesign = document.createElement('p')
-            pillDesign.classList.add('col-2', 'badge', 'rounded-pill')
-            pillDesign.classList.add('text-bg-primary')
+            pillDesign.classList.add('text-bg-light')
             pillDesign.innerHTML = 'Fizz'
             divEl.appendChild(pillDesign);
+
         } else if ((i % 5 === 0) && (i % 3 !== 0)) {
-            console.log('Buzz')
+            pillDesign.classList.add('text-bg-dark')
+            pillDesign.innerHTML = 'Buzz'
+            divEl.appendChild(pillDesign);
+
         } else if ((i % 5 === 0) && (i % 3 === 0)) {
-            console.log('FizzBuzz')
+            pillDesign.classList.add('text-bg-danger')
+            pillDesign.innerHTML = 'FizzBuzz'
+            divEl.appendChild(pillDesign);
+
         } else {
             console.log(i)
+            pillDesign.classList.add('text-bg-secondary')
+            pillDesign.innerHTML = i
+            divEl.appendChild(pillDesign);
         }
+
     }
 
 })
